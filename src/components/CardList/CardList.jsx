41ca012461dcf3ed7a8159b3e079605d75172d17
Pavlo Berkut <pavlo.berkut@gmail.com>
2023-04-23 17:12:55 +0300

    update

--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -1,10 +1,9 @@
 import { CardItem } from '../CardItem';
-// import css from './CardList.module.css';
+import css from './CardList.module.css';
 
 const CardList = ({ users }) => {
   return (
-    // className={css.list}
-    <ul>
+    <ul className={css.list}>
       {users.map(user => (
         <li key={user.id}>
           <CardItem user={user} />
